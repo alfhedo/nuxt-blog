abe54533e801db567c3176f931fca1ef9190e0be
Alfhedo <jordyalfredob@gmail.com>
2021-09-13 10:51:38 -0700

    Usar componente bootstrap-vue para preloader

--- components/loading.vue
+++ components/loading.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div(v-if="loading" class="splash-screen")
     div(class="spinner-wrapper")
-      div(class="spinner")    
+      b-spinner(type="grow")
 </template>
 
 <script>
@@ -9,14 +9,6 @@
     data: () => ({
       loading: true
     }),
-    methods: {
-      start() {
-        this.loading = true
-      },
-      finish() {
-        this.loading = false
-      }
-    },
     mounted(){
       this.loading = false
     }
@@ -38,37 +30,5 @@
   top: 50%;
 
   transform: translate(-50%, -50%);
-}
-.spinner {
-  width: 80px;
-  height: 80px;
-  margin: 100px auto;
-  background-color: $text-primary;
-
-  border-radius: 100%;
-  -webkit-animation: sk-scaleout 1s infinite ease-in-out;
-  animation: sk-scaleout 1s infinite ease-in-out;
-}
-
-@-webkit-keyframes sk-scaleout {
-  0% {
-    -webkit-transform: scale(0);
-  }
-  100% {
-    -webkit-transform: scale(1);
-    opacity: 0;
-  }
-}
-
-@keyframes sk-scaleout {
-  0% {
-    -webkit-transform: scale(0);
-    transform: scale(0);
-  }
-  100% {
-    -webkit-transform: scale(1);
-    transform: scale(1);
-    opacity: 0;
-  }
 }
 </style>
