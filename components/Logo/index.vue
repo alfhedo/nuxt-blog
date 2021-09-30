3155df8ce9a2ae5135096a2c83689a37d4463666
Alfhedo <jordyalfredob@gmail.com>
2021-09-30 16:14:36 -0700

    Convertir logo css en componente

---
+++ components/Logo/index.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+#divtorotate
+  svg(viewBox='0 0 500 200')
+    path(d='M0,150 C160,-40 350,240 500,55 L660,190 L0,350 Z'
+    class="svg-stroke")
+  span#span1
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'logo',
+})
+</script>
+
+<style lang="scss" scoped>
+#divtorotate {
+  width:900px;
+  height:900px;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  border-radius:50%;
+  overflow:hidden;
+  transform:rotate(-15deg)
+}
+#divtorotate:before{    
+  content:'';
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  display:absolute;
+  width:900px;
+  height:900px;
+  background:linear-gradient(to bottom,#DB272F 60%,#56595A 40%);
+  border-radius:50%;    
+}
+
+#span1{	  
+  display:block;  
+  height:300px;
+  width:300px;
+  border-radius:50%;
+  background:$background-color;
+  position:absolute;
+  top:50%;
+  left:50%;
+	transform:translate(-50%,-50%);    
+  z-index:10;
+}
+svg {  
+  position: absolute;
+  top: 290px;
+  left: 0;
+}
+.svg-stroke{
+  stroke: $background-color;
+  stroke-width: 14;
+  fill:#56595A;  
+}  
+</style>
--- components/loading.vue
+++ components/loading.vue
@@ -1,13 +1,9 @@
 <template lang="pug">
   div(v-if="loading" class="splash-screen")
-    div(class="spinner-wrapper")
-      img(class="px150" src="~/assets/images/petsa (1)-min.png")
-      #divtorotate
-        svg(viewBox='0 0 500 200' preserveAspectRatio='xMinYMin meet')
-          path(d='M0,150 C160,-40 260,240 500,45 L660,190 L0,350 Z'
-          class="svgStroke")
-        span#span1
+    div(class="spinner-wrapper")      
+      logo(class="")     
       //- b-spinner(type="grow")
+      //- img(class="px150" src="~/assets/images/petsa (1)-min.png") 
 </template>
 
 <script>
@@ -43,51 +39,7 @@
   -webkit-animation: spin 1s linear infinite;
   -moz-animation: spin 1s;
 }
-#divtorotate {
-  width:150px;
-  height:150px;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  border-radius:50%;
-  overflow:hidden;
-  transform:rotate(-15deg)
-}
-#divtorotate:before{    
-  content:'';
-  position:absolute;
-  top:50%;
-  left:50%;
-  transform:translate(-50%,-50%);
-  display:absolute;
-  width:150px;
-  height:150px;
-  background:linear-gradient(to bottom,#DB272F 60%,#56595A 40%);
-  border-radius:50%;    
-}
 
-#span1{	  
-  display:block;  
-  height:52px;
-  width:52px;
-  border-radius:50%;
-  background:$background-color;
-  position:absolute;
-  top:50%;
-  left:50%;
-	transform:translate(-50%,-50%);    
-  z-index:10;
-}
-svg {  
-  position: absolute;
-  top: 48px;
-  left: 0;
-}
-.svgStroke{
-  stroke: $background-color;
-  stroke-width: 14;
-  fill:#56595A;  
-}
 .px150{
   width: 150px;
   height: 150px;  
